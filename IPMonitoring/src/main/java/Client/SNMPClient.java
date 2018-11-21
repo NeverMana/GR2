5282bbcd6ec942fdde4ddaeba6eb7047db5dd26f
NeverMana <jmsd.jose.dias@gmail.com>
2018-11-21 19:29:02 +0000

    uh... walk so de iftable, copiado de um exemplo para depois usar como base

--- IPMonitoring/src/main/java/Client/SNMPClient.java
+++ IPMonitoring/src/main/java/Client/SNMPClient.java
@@ -1,5 +1,8 @@
 package Client;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.*;
+
 
 import org.snmp4j.CommunityTarget;
 import org.snmp4j.PDU;
@@ -14,6 +17,9 @@ import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.DefaultPDUFactory;
+import org.snmp4j.util.TreeEvent;
+import org.snmp4j.util.TreeUtils;
 
 public class SNMPClient {
 
@@ -26,12 +32,109 @@ public class SNMPClient {
     }
 
     public static void main(String[] args) throws IOException {
+        Scanner input;
+        String add, ip, porta, sysDescr, oid;
+        SNMPClient client;
 
-        SNMPClient client = new SNMPClient("udp:127.0.0.1/5004");
+        CommunityTarget target = new CommunityTarget();
+
+        System.out.println("Indique ip do agente:");
+        input = new Scanner(System. in);
+        ip = input. nextLine(); //formato::  "127.0.0.1"
+        System.out.println("Indique porta:");
+        porta = input.nextLine();//int
+        add = "udp:"+ip+"/"+porta;
+
+
+        client = new SNMPClient(add);
         client.start();
 
-        //String sysDescr = client.getAsString(new OID(".1.3.6.1.2.1.1.1.0")); sysDescr
-        System.out.println();
+
+        System.out.println("Indique OID:");
+        /*
+        * OIDs:
+        * ".1.3.6.1.2.1.1.1.0" - sysdescription
+        * ".1.3.6.1.2.1.2.2" ifTable
+        */
+        oid= input.nextLine();
+        target = client.configTarget(target,add);
+        int op=0;
+        switch (op){
+            case 0:{
+                Map<String, String> result = client.doWalk(oid, target); // ifTable, mib-2 interfaces
+                client.printWalk(result);
+            }
+            case 1:{
+                String val = client.getAsString(new OID(oid));
+                System.out.println(val);
+            }
+        }
+
+
+
+    }
+
+    private void printWalk(Map<String, String> result) throws IOException {
+
+        for (Map.Entry<String, String> entry : result.entrySet()) {
+            if (entry.getKey().startsWith(".1.3.6.1.2.1.2.2.1.2.")) {
+                System.out.println("ifDescr" + entry.getKey().replace(".1.3.6.1.2.1.2.2.1.2", "") + ": " + entry.getValue());
+            }
+            if (entry.getKey().startsWith(".1.3.6.1.2.1.2.2.1.3.")) {
+                System.out.println("ifType" + entry.getKey().replace(".1.3.6.1.2.1.2.2.1.3", "") + ": " + entry.getValue());
+            }
+        }
+    }
+
+    private CommunityTarget configTarget(CommunityTarget target, String add) {
+        target.setCommunity(new OctetString("public"));
+        target.setAddress(GenericAddress.parse(add));
+        target.setRetries(2);
+        target.setTimeout(1500);
+        target.setVersion(SnmpConstants.version2c);
+        return target;
+    }
+
+    /*
+        TransportMapping<? extends Address> transport = new DefaultUdpTransportMapping();
+        Snmp snmp = new Snmp(transport);
+        transport.listen();
+     */
+
+    public Map<String, String> doWalk(String tableOid, Target target) throws IOException {
+        Map<String, String> result = new TreeMap<String, String>();
+
+        TreeUtils treeUtils = new TreeUtils(snmp, new DefaultPDUFactory());
+        List<TreeEvent> events = treeUtils.getSubtree(target, new OID(tableOid));
+        if (events == null || events.size() == 0) {
+            System.out.println("Error: Unable to read table...");
+            return result;
+        }
+
+        for (TreeEvent event : events) {
+            if (event == null) {
+                continue;
+            }
+            if (event.isError()) {
+                System.out.println("Error: table OID [" + tableOid + "] " + event.getErrorMessage());
+                continue;
+            }
+
+            VariableBinding[] varBindings = event.getVariableBindings();
+            if (varBindings == null || varBindings.length == 0) {
+                continue;
+            }
+            for (VariableBinding varBinding : varBindings) {
+                if (varBinding == null) {
+                    continue;
+                }
+
+                result.put("." + varBinding.getOid().toString(), varBinding.getVariable().toString());
+            }
+
+        }
+
+        return result;
     }
 
     private void start() throws IOException {
